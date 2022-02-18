769713fa441d901eab741119e5fd11c1998de648
Diego Mucheniski <mucheniski@gmail.com>
2022-02-18 10:46:42 -0300

    First commit

---
+++ src/main/java/com/example/WebTestClientExample/WebTestClientExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.WebTestClientExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebTestClientExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebTestClientExampleApplication.class, args);
+	}
+
+}
